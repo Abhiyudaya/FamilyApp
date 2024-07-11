e5b123fe897e4586f1e90012d9c0a2cce4ea3cb0
Abhiyudaya <121346347+Abhiyudaya@users.noreply.github.com>
2024-07-11 14:51:47 +0530

    1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 //    kotlin("kapt") version "2.0.0"
 //    id("com.google.devtools.ksp")
-    id("kotlin-kapt")
-    id("com.google.devtools.ksp")
+//    id("kotlin-kapt")
+//    id("com.google.devtools.ksp")
 }
 
 android {
@@ -54,33 +54,33 @@ dependencies {
 
     //Coroutines
     implementation(libs.kotlinx.coroutines.android)
-
-    //Room DB
-    val room_version = "2.6.1"
-
-    implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
-
-    // To use Kotlin annotation processing tool (kapt)
-    kapt("androidx.room:room-compiler:$room_version")
-    // To use Kotlin Symbol Processing (KSP)
-    ksp("androidx.room:room-compiler:$room_version")
-
-    // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:$room_version")
-
-    // optional - RxJava2 support for Room
-    implementation("androidx.room:room-rxjava2:$room_version")
-
-    // optional - RxJava3 support for Room
-    implementation("androidx.room:room-rxjava3:$room_version")
-
-    // optional - Guava support for Room, including Optional and ListenableFuture
-    implementation("androidx.room:room-guava:$room_version")
-
-    // optional - Test helpers
-    testImplementation("androidx.room:room-testing:$room_version")
-
-    // optional - Paging 3 Integration
-    implementation("androidx.room:room-paging:$room_version")
+//
+//    //Room DB
+//    val room_version = "2.6.1"
+//
+//    implementation("androidx.room:room-runtime:$room_version")
+//    annotationProcessor("androidx.room:room-compiler:$room_version")
+//
+//    // To use Kotlin annotation processing tool (kapt)
+//    kapt("androidx.room:room-compiler:$room_version")
+//    // To use Kotlin Symbol Processing (KSP)
+//    ksp("androidx.room:room-compiler:$room_version")
+//
+//    // optional - Kotlin Extensions and Coroutines support for Room
+//    implementation("androidx.room:room-ktx:$room_version")
+//
+//    // optional - RxJava2 support for Room
+//    implementation("androidx.room:room-rxjava2:$room_version")
+//
+//    // optional - RxJava3 support for Room
+//    implementation("androidx.room:room-rxjava3:$room_version")
+//
+//    // optional - Guava support for Room, including Optional and ListenableFuture
+//    implementation("androidx.room:room-guava:$room_version")
+//
+//    // optional - Test helpers
+//    testImplementation("androidx.room:room-testing:$room_version")
+//
+//    // optional - Paging 3 Integration
+//    implementation("androidx.room:room-paging:$room_version")
 }
